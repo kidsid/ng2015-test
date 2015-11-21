fc6e13fe66ef6ac970e90c1490c185237efd5be8
kidsid <gigagelashvil@gmail.com>
2015-11-21 14:16:21 +0400

    hello ng2015

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,9 @@ import {bootstrap, Component} from 'angular2/angular2';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First ng2015 app, hell yeah</h1>'
+    template: '<h1>My First ng2015 app, hell yeah' +
+    '<br/>second try' +
+    '<br/>third</h1>'
 })
 
 class AppComponent{}
